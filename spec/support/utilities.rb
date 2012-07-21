2904c3da2eac81d2b75bf2c5bf7edcf81e62b9ba
maria <maria@dummy.org.es>
2012-07-21 14:00:16 +0200

    Fixed again

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-include ApplicationHelper
+#<<<<<<< HEAD
+#include ApplicationHelper
 #def full_title (page_title)
 #	base_title = "Ruby on Rails Tutorial Sample App"
 #	if page_title.empty?
@@ -8,13 +8,13 @@ include ApplicationHelper
 #		"#{base_title} | #{page_title}"
 #	end
 #end
-=======
-def full_title (page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
->>>>>>> 3381235b011a8dc8ed7595017b1bcbab7690b194
+#======
+#def full_title (page_title)
+#	base_title = "Ruby on Rails Tutorial Sample App"
+#	if page_title.empty?
+#		base_title
+#	else
+#		"#{base_title} | #{page_title}"
+#		end
+#	end
+#>>>>>>> 3381235b011a8dc8ed7595017b1bcbab7690b194
