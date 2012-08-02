5ac2c4e376494095552b242f1dc92103a93d90d0
maria <maria@dummy.org.es>
2012-08-02 11:44:13 +0200

    Signin Exercises done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,23 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
-		user = User.find_by_email(params[:session][:email])
-		if user && user.authenticate(params[:session][:password])
+		
+		if (params[:session].nil?)
+			user_email = params[:email]
+			user_password = params[:password]
+		else
+			user_email = params[:session][:email]
+			user_password = params[:session][:password]
+		end
+		#puts ".............................................................."
+		#puts user_email
+		#puts user_password
+		#puts ".............................................................."
+
+		#user = User.find_by_email(params[:session][:email])
+		user = User.find_by_email(user_email)
+		#if user && user.authenticate(params[:session][:password])
+		if user && user.authenticate(user_password)
 			# Sign the user in and redirect to the user's show page.
 			sign_in user
 			redirect_to user
